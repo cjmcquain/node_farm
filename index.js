4800ef8be667166ea505529f9d715c839c21d99f
Charles McQuain <cmcquai1@my.westga.edu>
2019-08-08 22:45:17 -0700

    Added route handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //////////////////////////////
 // FILES
@@ -32,7 +33,16 @@ console.log('Will read file!');
 //////////////////////////////
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.end('Page Not Found!');
+  }
+
 });
 
 server.listen(8000, '127.0.0.1', () => {
